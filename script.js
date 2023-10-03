9893e818ed07d4770e8b6f80ec3ad297078e6950
codec-onion <bonnetmatthieu86@gmail.com>
2023-10-03 15:45:07 +0200

    added glass capacity selection

--- script.js
+++ script.js
@@ -1,49 +1,72 @@
-let glassCounter = 0;
-let waterQuantity = 0;
+let glassCounter = 0
+let waterQuantity = 0
+let glassCapacity = 0
 
-const waterGlassDisplay = document.querySelector(".water-glass");
-waterGlassDisplay.innerText = "0 verre";
+function setGlassQuantity() {
+  document
+    .querySelector('.glass-capacity-button')
+    .addEventListener('click', () => {
+      const glassCapacityInput = document.querySelector('.glass-capacity-input')
+      glassCapacity = glassCapacityInput.value
+      document.querySelector('.glass-capacity-display').innerText =
+        glassCapacity
+    })
+}
+setGlassQuantity()
+
+const waterGlassDisplay = document.querySelector('.water-glass')
+if (glassCounter <= 1) {
+  waterGlassDisplay.innerText = `${glassCounter} verre`
+} else {
+  waterGlassDisplay.innerText = `${glassCounter} verres`
+}
 
 function setWaterGlassDisplay() {
-  document.querySelector(".add-half-glass").addEventListener("click", () => {
-    glassCounter = glassCounter + 0.5;
+  document.querySelector('.add-half-glass').addEventListener('click', () => {
+    glassCounter = glassCounter + 0.5
     if (glassCounter <= 1) {
-      waterGlassDisplay.innerText = `${glassCounter} verre`;
+      waterGlassDisplay.innerText = `${glassCounter} verre`
     } else {
-      waterGlassDisplay.innerText = `${glassCounter} verres`;
+      waterGlassDisplay.innerText = `${glassCounter} verres`
     }
-    setWaterQuantityDisplay();
-  });
-  document.querySelector(".add-a-glass").addEventListener("click", () => {
-    glassCounter++;
+    setWaterQuantityDisplay()
+  })
+  document.querySelector('.add-a-glass').addEventListener('click', () => {
+    glassCounter++
     if (glassCounter <= 1) {
-      waterGlassDisplay.innerText = `${glassCounter} verre`;
+      waterGlassDisplay.innerText = `${glassCounter} verre`
     } else {
-      waterGlassDisplay.innerText = `${glassCounter} verres`;
+      waterGlassDisplay.innerText = `${glassCounter} verres`
     }
-    setWaterQuantityDisplay();
-  });
+    setWaterQuantityDisplay()
+  })
 }
-setWaterGlassDisplay();
+setWaterGlassDisplay()
 
-const waterQuantityDisplay = document.querySelector(".water-quantity");
-waterQuantityDisplay.innerText = "0 centilitre.";
+const waterQuantityDisplay = document.querySelector('.water-quantity')
+if (waterQuantity === 0 || waterQuantity === 1) {
+  waterQuantityDisplay.innerText = `${waterQuantity} centilitre`
+} else if (waterQuantity < 100) {
+  waterQuantityDisplay.innerText = `${waterQuantity} centilitres`
+} else {
+  waterQuantityDisplay.innerText = `${waterQuantity / 100} litres`
+}
 
 function setWaterQuantityDisplay() {
-  waterQuantity = glassCounter * 30;
+  waterQuantity = glassCounter * glassCapacity
   if (waterQuantity < 100) {
-    waterQuantityDisplay.innerText = `${waterQuantity} centilitres.`;
+    waterQuantityDisplay.innerText = `${waterQuantity} centilitres`
   } else {
-    waterQuantityDisplay.innerText = `${waterQuantity / 100} litres.`;
+    waterQuantityDisplay.innerText = `${waterQuantity / 100} litres`
   }
 }
 
 function reset() {
-  document.querySelector(".reset").addEventListener("click", () => {
-    glassCounter = 0;
-    waterQuantity = 0;
-    waterGlassDisplay.innerText = glassCounter;
-    waterQuantityDisplay.innerText = `${waterQuantity} centilitre.`;
-  });
+  document.querySelector('.reset').addEventListener('click', () => {
+    glassCounter = 0
+    waterQuantity = 0
+    waterGlassDisplay.innerText = glassCounter
+    waterQuantityDisplay.innerText = `${waterQuantity} centilitre`
+  })
 }
-reset();
+reset()
